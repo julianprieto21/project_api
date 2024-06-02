8a6f86f3fab469dd7aac268d21d47000ba6a365a
Julian Prieto <prietojulian2003@gmail.com>
2024-06-02 16:03:24 -0300

    visual changes and routing

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,8 +1,8 @@
 export const Game = ({ game }: { game: any }) => {
   return (
-    <div className="mt-10">
-      <picture className="relative">
-        <a href={`/games/${game.id}`}>
+    <div className="mt-10 w-[460px]">
+      <picture className="relative w-full grid place-content-center">
+        <a href={`/${game.id}`}>
           <img
             className="rounded-lg aspect-video md:hover:scale-105 transition"
             src={game.imageUrl}
@@ -10,11 +10,17 @@ export const Game = ({ game }: { game: any }) => {
           />
         </a>
       </picture>
-      <h2 className="w-full text-left text-4xl font-semibold pt-3">
+      <h2
+        className="w-full text-left text-4xl font-semibold pt-3 truncate"
+        title={game.name}
+      >
         {game.name}
       </h2>
-      <h3 className="w-full text-left text-xl font-base text-neutral-600">
-        {game.developers}
+      <h3
+        className="w-full text-left text-xl font-base text-neutral-600 truncate"
+        title={game.developers.join(", ")}
+      >
+        {game.developers.join(", ")}
       </h3>
     </div>
   );
