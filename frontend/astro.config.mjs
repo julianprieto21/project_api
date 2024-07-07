9dfa13ee5502a59a9520e9de946fb07842284269
Julian Prieto <prietojulian2003@gmail.com>
2024-07-06 23:08:54 -0300

    add node adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
   experimental: {
-    actions: true,
+    actions: true
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
